6b020fc75c359e0d084ceb6d9ca944eb1a4d5fc4
pipersgo <109430979+pipersgo@users.noreply.github.com>
2025-11-09 18:37:21 +0000

    ci: format documents

--- templates/turbo-with-tailwind-shadcn/packages/ui/src/components/counter.tsx
+++ templates/turbo-with-tailwind-shadcn/packages/ui/src/components/counter.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button } from "./ui/button";
 
 export const Counter: React.FC<React.ComponentProps<typeof Button>> = (
-  props
+  props,
 ) => {
   const [count, setCount] = useState(0);
 
